4e499644e9aa9c040a6c4ff1f12c6d2ce20ef9db
Logan-Wessel <lgwessel812@gmail.com>
2024-05-22 05:33:38 -0500

    Update settings.lua

---
+++ lua/custom/settings.lua
@@ -0,0 +1,56 @@
+-- Custom settings
+
+-- Set colorscheme to mooonfly
+vim.cmd [[colorscheme moonfly]]
+
+-- Set options
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+vim.opt.termguicolors = true
+
+vim.opt.isfname:append '@-@'
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = '160'
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set keymaps
+vim.keymap.set('x', '<leader>p', '"_dp')
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
+vim.keymap.set('i', '<C-c>', '<Esc>')
+
+vim.keymap.set('n', 'Q', '<nop>')
+
+vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>')
+
+-- Go specific error handling
+vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR>}<Esc>Oreturn err<Esc>')
+
+-- Lsp remaps?
+
+-- Enable inlay hints
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = {
+    -- prefix = '',
+    spacing = 0,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+})
